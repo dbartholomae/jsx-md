7fd48977503ec6d13140b653913116e4aa42c027
Daniel Bartholomae <daniel@bartholomae.name>
2020-08-23 22:25:23 +0200

    refactor: remove unnecessary async

--- src/render.test.tsx
+++ src/render.test.tsx
@@ -3,18 +3,18 @@
 import { render } from './render'
 
 describe('render', () => {
-  it('renders a text node to its content', async () => {
+  it('renders a text node to its content', () => {
     expect(<md-text>Content</md-text>).toEqual('Content')
   })
 
-  it('renders a nested component to its content', async () => {
+  it('renders a nested component to its content', () => {
     function Text ({ children }: { children?: string }) {
       return children
     }
     expect(<Text>Content</Text>).toEqual('Content')
   })
 
-  it('renders a functional component to its content', async () => {
+  it('renders a functional component to its content', () => {
     function Text ({ children }: { children?: string }) {
       return children
     }
@@ -25,7 +25,7 @@ describe('render', () => {
     expect(<TestComponent />).toEqual('Content')
   })
 
-  it('renders a text node with multiple children to its content', async () => {
+  it('renders a text node with multiple children to its content', () => {
     expect(<md-text>{'More'} {'Content'}</md-text>).toEqual('More Content')
   })
 })

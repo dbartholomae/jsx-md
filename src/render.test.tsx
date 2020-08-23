f617a020e862386231afb85a5ffcc52048fe6aa1
Daniel Bartholomae <daniel@bartholomae.name>
2020-08-23 22:21:15 +0200

    fix: correctly render multiple children

--- src/render.test.tsx
+++ src/render.test.tsx
@@ -1,22 +1,31 @@
 /* @jsx render */
 
 import { render } from './render'
-import { Text } from './elements'
 
 describe('render', () => {
   it('renders a text node to its content', async () => {
     expect(<md-text>Content</md-text>).toEqual('Content')
   })
 
-  it('renders a text component to its content', async () => {
+  it('renders a nested component to its content', async () => {
+    function Text ({ children }: { children?: string }) {
+      return children
+    }
     expect(<Text>Content</Text>).toEqual('Content')
   })
 
   it('renders a functional component to its content', async () => {
+    function Text ({ children }: { children?: string }) {
+      return children
+    }
     function TestComponent () {
       return <Text>Content</Text>
     }
 
     expect(<TestComponent />).toEqual('Content')
+  })
+
+  it('renders a text node with multiple children to its content', async () => {
+    expect(<md-text>{'More'} {'Content'}</md-text>).toEqual('More Content')
   })
 })
--- src/render.ts
+++ src/render.ts
@@ -2,11 +2,11 @@ type Props = {
   [key: string]: any
 }
 
-export function render(functionalComponent: (props: Props) => string, attributes: null, children: undefined): string
+export function render(functionalComponent: (props: Props) => string, attributes: null): string
 export function render (nodeType: string, attributes: null, children: string): string
-export function render (typeOrComponent: string | ((props: Props) => string), attributes: null, children?: string): string {
+export function render (typeOrComponent: string | ((props: Props) => string), attributes: null, ...children: string[]): string {
   if (typeof typeOrComponent === 'function') {
-    return typeOrComponent({ children })
+    return typeOrComponent({ children: children.join('') })
   }
-  return children!
+  return children.join('')
 }

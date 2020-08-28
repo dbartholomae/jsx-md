8465e493dbbb09bb503f8ff8ef8f4caa6668db3e
Daniel Bartholomae <daniel@bartholomae.name>
2020-08-29 00:20:38 +0200

    fix: limit Header to 6 levels
    That's the maximum on Github

--- src/elements/Heading.tsx
+++ src/elements/Heading.tsx
@@ -3,7 +3,7 @@ import MD, { Fragment } from "..";
 
 type Props = {
   children?: string | string[];
-  level: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+  level: 1 | 2 | 3 | 4 | 5 | 6;
 };
 
 export function Heading({ children, level }: Props) {

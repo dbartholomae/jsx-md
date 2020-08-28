70a2e72b8bc50a02df40f92d8c3bbfd8dcd565c1
Daniel Bartholomae <daniel@bartholomae.name>
2020-08-29 00:00:34 +0200

    test: avoid jsxFrag
    It causes trouble with my IDE
    
    See https://youtrack.jetbrains.com/issue/WEB-47245

--- src/elements/Fragment.test.tsx
+++ src/elements/Fragment.test.tsx
@@ -1,10 +1,9 @@
 /* @jsx MD */
-/* @jsxFrag Fragment */
 import MD, { render } from "..";
 import { Fragment } from ".";
 
 describe("Fragment", () => {
   it("renders a single component", () => {
-    expect(render(<>Test</>)).toEqual("Test");
+    expect(render(<Fragment>Test</Fragment>)).toEqual("Test");
   });
 });

dd8b7266d543f4e98519b264a1d4e1d9f6657190
Daniel Bartholomae <daniel@bartholomae.name>
2020-08-29 00:38:00 +0200

    fix: remove linebreak in front of codeblock

--- src/elements/CodeBlock.test.tsx
+++ src/elements/CodeBlock.test.tsx
@@ -5,12 +5,12 @@ import { CodeBlock } from ".";
 
 describe("CodeBlock", () => {
   it("returns the wrapped content", () => {
-    expect(render(<CodeBlock>Test</CodeBlock>)).toBe("\n```\nTest\n```\n");
+    expect(render(<CodeBlock>Test</CodeBlock>)).toBe("```\nTest\n```\n");
   });
 
   it("returns the wrapped content with a language", () => {
     expect(render(<CodeBlock language="typescript">Test</CodeBlock>)).toBe(
-      "\n```typescript\nTest\n```\n"
+      "```typescript\nTest\n```\n"
     );
   });
 });
--- src/elements/CodeBlock.tsx
+++ src/elements/CodeBlock.tsx
@@ -6,7 +6,7 @@ type Props = { children?: string; language?: string };
 export function CodeBlock({ children, language }: Props) {
   return (
     <Fragment>
-      {"\n"}```{language}
+      ```{language}
       {"\n"}
       {children}
       {"\n"}```{"\n"}

b57d7b8976328bc933232a6b0dae2ad868bb5443
Daniel Bartholomae <daniel@bartholomae.name>
2020-08-29 00:49:15 +0200

    docs: write README.md

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # jsx-md
 
+[![npm version](https://badge.fury.io/js/jsx-md.svg)](https://npmjs.org/package/jsx-md)
+[![downloads](https://img.shields.io/npm/dw/jsx-md.svg)](https://npmjs.org/package/jsx-md)
+[![open issues](https://img.shields.io/github/issues-raw/dbartholomae/jsx-md.svg)](https://github.com/dbartholomae/jsx-md/issues)
+[![build status](https://github.com/dbartholomae/jsx-md/workflows/.github/workflows/build.yml/badge.svg?branch=main)](https://github.com/dbartholomae/jsx-md/actions?query=workflow%3A.github%2Fworkflows%2Fbuild.yml)
+[![codecov](https://codecov.io/gh/dbartholomae/jsx-md/branch/main/graph/badge.svg)](https://codecov.io/gh/dbartholomae/jsx-md)
+[![dependency status](https://david-dm.org/dbartholomae/jsx-md.svg?theme=shields.io)](https://david-dm.org/dbartholomae/jsx-md)
+[![devDependency status](https://david-dm.org/dbartholomae/jsx-md/dev-status.svg)](https://david-dm.org/dbartholomae/jsx-md?type=dev)
+
 Generate markdown files with a React\-like syntax.
+
+## Usage
+
+```tsx
+// We need to tell the JSX transpiler that in this file,
+// instead of React we use the custom createElement and Fragment
+// functions from jsx-md
+/* @jsx MD */
+/* @jsxFrag Fragment */
+import MD, { Fragment, Heading, LineBreak, render, Text } from "jsx-md";
+import { writeFileSync } from "fs";
+import pkg from "./package.json";
+
+function Readme() {
+  return (
+    <>
+      <Heading level={1}>{pkg.name}</Heading>
+      <Text>{pkg.description}</Text>
+      <LineBreak />
+    </>
+  );
+}
+
+// Currently, the rendering function is not always necessary.
+// This might change in the future, though, so do not rely on any
+// intermediate results from creating elements.
+writeFileSync("README.md", render(<Readme />));
+```
--- examples/README.md.tsx
+++ examples/README.md.tsx
@@ -1,5 +1,13 @@
 /* @jsx MD */
-import MD, { Fragment, Heading, LineBreak, Text } from "..";
+import MD, {
+  CodeBlock,
+  Fragment,
+  Heading,
+  Image,
+  LineBreak,
+  Link,
+  Text,
+} from "..";
 import { writeFileSync } from "fs";
 import pkg from "../package.json";
 
@@ -7,8 +15,74 @@ function Readme() {
   return (
     <Fragment>
       <Heading level={1}>{pkg.name}</Heading>
+      <Link to="https://npmjs.org/package/jsx-md">
+        <Image src="https://badge.fury.io/js/jsx-md.svg">npm version</Image>
+      </Link>
+      <LineBreak />
+      <Link to="https://npmjs.org/package/jsx-md">
+        <Image src="https://img.shields.io/npm/dw/jsx-md.svg">downloads</Image>
+      </Link>
+      <LineBreak />
+      <Link to="https://github.com/dbartholomae/jsx-md/issues">
+        <Image src="https://img.shields.io/github/issues-raw/dbartholomae/jsx-md.svg">
+          open issues
+        </Image>
+      </Link>
+      <LineBreak />
+      <Link to="https://github.com/dbartholomae/jsx-md/actions?query=workflow%3A.github%2Fworkflows%2Fbuild.yml">
+        <Image src="https://github.com/dbartholomae/jsx-md/workflows/.github/workflows/build.yml/badge.svg?branch=main">
+          build status
+        </Image>
+      </Link>
+      <LineBreak />
+      <Link to="https://codecov.io/gh/dbartholomae/jsx-md">
+        <Image src="https://codecov.io/gh/dbartholomae/jsx-md/branch/main/graph/badge.svg">
+          codecov
+        </Image>
+      </Link>
+      <LineBreak />
+      <Link to="https://david-dm.org/dbartholomae/jsx-md">
+        <Image src="https://david-dm.org/dbartholomae/jsx-md.svg?theme=shields.io">
+          dependency status
+        </Image>
+      </Link>
+      <LineBreak />
+      <Link to="https://david-dm.org/dbartholomae/jsx-md?type=dev">
+        <Image src="https://david-dm.org/dbartholomae/jsx-md/dev-status.svg">
+          devDependency status
+        </Image>
+      </Link>
+      <LineBreak />
+      <LineBreak />
       <Text>{pkg.description}</Text>
       <LineBreak />
+      <LineBreak />
+      <Heading level={2}>Usage</Heading>
+      <CodeBlock language="tsx">
+        {`// We need to tell the JSX transpiler that in this file,
+// instead of React we use the custom createElement and Fragment
+// functions from jsx-md
+/* @jsx MD */
+/* @jsxFrag Fragment */
+import MD, { Fragment, Heading, LineBreak, render, Text } from "jsx-md";
+import { writeFileSync } from "fs";
+import pkg from "./package.json";
+
+function Readme() {
+  return (
+    <>
+      <Heading level={1}>{pkg.name}</Heading>
+      <Text>{pkg.description}</Text>
+      <LineBreak />
+    </>
+  );
+}
+
+// Currently, the rendering function is not always necessary.
+// This might change in the future, though, so do not rely on any
+// intermediate results from creating elements.
+writeFileSync("README.md", render(<Readme />));`}
+      </CodeBlock>
     </Fragment>
   );
 }

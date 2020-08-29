99a8046b3fe4cdeccd52ba9ac19b87b68a962de4
Daniel Bartholomae <daniel@bartholomae.name>
2020-08-29 02:48:01 +0200

    test: create copy fixture for integration test
    Previously the main README file was used for testing. This meant that each change to the readme required a change to the test. Decouple test and readme.

--- examples/README.md.test.tsx
+++ examples/README.md.test.tsx
@@ -3,7 +3,9 @@ import "./README.md.tsx";
 
 describe("README creation script", () => {
   it("creates the README file of the repo", async () => {
-    const expectedFile = fs.readFileSync("./README.md", { encoding: "utf8" });
+    const expectedFile = fs.readFileSync("./examples/README.expected.md", {
+      encoding: "utf8",
+    });
     const actualFile = fs.readFileSync("./examples/README.md", {
       encoding: "utf8",
     });

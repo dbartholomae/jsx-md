05e7df7fc82b567c892704640f490fd1bd0b5162
Daniel Bartholomae <daniel@bartholomae.name>
2020-09-07 18:47:18 +0200

    refactor: simplify Heading code

--- src/elements/Heading.tsx
+++ src/elements/Heading.tsx
@@ -1,12 +1,14 @@
 /* @jsx MD */
-import MD, { Component, Fragment, MarkdownNode } from "..";
+import MD, { Component, Fragment, PropsWithChildren } from "..";
 
 type Props = {
-  children?: MarkdownNode;
   level: 1 | 2 | 3 | 4 | 5 | 6;
 };
 
-export const Heading: Component<Props> = ({ children, level }) => (
+export const Heading: Component<PropsWithChildren<Props>> = ({
+  children,
+  level,
+}) => (
   <Fragment>
     {"#".repeat(level)} {children}
     {"\n\n"}

283c86876b96bd126877a31b81b3b5f9b9c401e6
Daniel Bartholomae <daniel@bartholomae.name>
2020-09-07 18:07:12 +0200

    refactor: remove duplicate export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { createElement } from "./createElement";
 
 export * from "./elements";
-export * from "./elements/Fragment";
 export * from "./model";
 export * from "./render";
 export default createElement;

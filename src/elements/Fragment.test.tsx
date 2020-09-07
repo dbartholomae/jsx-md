6e8cb0b34abb2dacfca50bd61813f6e662c54718
Daniel Bartholomae <daniel@bartholomae.name>
2020-09-07 22:20:17 +0200

    test: use jsx fragment in Fragment test

--- src/elements/Fragment.test.tsx
+++ src/elements/Fragment.test.tsx
@@ -1,9 +1,14 @@
 /* @jsx MD */
+/* @jsxFrag Fragment */
 import MD, { render } from "..";
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// It is used via @jsxFrag
 import { Fragment } from ".";
+/* eslint-enable @typescript-eslint/no-unused-vars */
 
 describe("Fragment", () => {
   it("renders a single component", () => {
-    expect(render(<Fragment>Test</Fragment>)).toEqual("Test");
+    expect(render(<>Test</>)).toEqual("Test");
   });
 });

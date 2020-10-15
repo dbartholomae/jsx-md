4bb8d9f0f50509aacebdb559d9d857636648de65
Daniel Bartholomae <daniel@bartholomae.name>
2020-10-15 23:42:06 +0200

    chore: improve release setup

--- release.config.js
+++ release.config.js
@@ -2,12 +2,21 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
     [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md", "docs", "package.json"],
+        assets: ["CHANGELOG.md", "README.md", "docs", "package.json"],
       },
     ],
     "@semantic-release/npm",
+    [
+      "@semantic-release/github",
+      {
+        addReleases: "top",
+        labels: ["chore"],
+        releasedLabels: false,
+      },
+    ],
   ],
 };
